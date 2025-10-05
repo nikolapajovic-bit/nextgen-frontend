f86d48430cfef003396094d3ff7221c843c6e185
nikolapajovic-bit <nikpajovic@gmail.com>
2025-10-05 21:37:05 +0200

    added axios

---
+++ src/api/axios.js
@@ -0,0 +1,25 @@
+import axios from "axios";
+
+const instance = axios.create({
+    baseURL: "http://192.168.100.6:8080"
+});
+
+instance.interceptors.request.use(config => {
+    const token = localStorage.getItem("token");
+    if(token) config.headers.Authorization = `Bearer ${token}`;
+    return config;
+});
+
+instance.interceptors.response.use(response => {
+    const newToken = response.headers['authorization'] || response.headers['Authorization'];
+    if(newToken && newToken.startsWith("Bearer ")) {
+        const token = newToken.substring(7);
+        localStorage.setItem("token", token);
+    }
+    return response;
+}, error => {
+        return Promise.reject(error);
+    }
+)
+
+export default instance;
